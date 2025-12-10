c3fd8f3d0a614fbfc5280fc714cda18bae9af091
LipoStyle <lipostylee@gmail.com>
2025-12-10 23:21:39 +0200

    Initial commit: Next.js gardening project finxing vars

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,10 @@
-import { Pool } from "pg";
+import pkg from "pg";
+const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: process.env.PGUSER!, // must be set in Vercel
-  password: process.env.PGPASSWORD!,
-  host: process.env.PGHOST!, // this must be your Supabase host
-  port: Number(process.env.PGPORT!),
-  database: process.env.PGDATABASE!,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: Number(process.env.PGPORT),
 });
